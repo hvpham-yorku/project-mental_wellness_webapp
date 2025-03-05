cbd7e6f76198744aef1a4ca9644950c09ec97ca6
4vantikanair <85316199+4vantikanair@users.noreply.github.com>
2025-03-05 18:42:15 -0500

    Backend directory is in MindSage directory

--- backend/utils.py
+++
@@ -1,6 +0,0 @@
-def validate_mood_data(data):
-    required_fields = ["happiness", "anxiety", "energy", "stress"]
-    for field in required_fields:
-        if field not in data:
-            return False
-    return True
